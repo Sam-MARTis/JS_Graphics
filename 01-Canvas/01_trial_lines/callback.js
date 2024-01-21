f0d23e71cbd37d676f3b82061e77b29045988bc8
Sam-MARTis <samanthmartis@gmail.com>
2024-01-21 09:36:07 +0530

    color working here

--- 01-Canvas/01_trial_lines/callback.js
+++ 01-Canvas/01_trial_lines/callback.js
@@ -22,18 +22,26 @@ c.moveTo(0,0);
 // c.lineTo(100, 200);
 // c.moveTo(200, 100);
 // c.stroke();
-const move = (x, y) => {
-    console.log(x, y);
+var prev_x = 0;
+var prev_y = 0;
+const move = (x, y, i) => {
+    console.log(x, y, i);
+    c.strokeStyle = `rgb(0, 0, ${25*i})`
+    c.beginPath();
+    c.moveTo(prev_x, prev_y);
+
     c.lineTo(x, y);
     c.stroke();
     c.moveTo(x, y);
+    prev_x = x;
+    prev_y = y;
 }
 
 
-const command = (func, x, y) => {
+const command = (func, x, y, i) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            func(x, y);
+            func(x, y, i);
             resolve();
         }, 2000);
     });
@@ -53,7 +61,8 @@ for (let i = 1; i <10; i++) {
     // a = a.then(()=>{command(move, (X*i), Y*i*i)}).then(() => {console.log("Done I guess!")});
     a = a.then(
             () => {
-                command(move, (X*i), Y*i*i)
+                command(move, (X*i), Y*i*i, i)
+                
             }
         )
         .then(
