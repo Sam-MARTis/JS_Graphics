430415c06acc949446e1ffd5d667313193f27486
Sam-MARTis <samanthmartis@gmail.com>
2024-01-21 10:46:11 +0530

    Done with color

--- 01-Canvas/03_lorentz_attractor/script.js
+++ 01-Canvas/03_lorentz_attractor/script.js
@@ -1,21 +1,51 @@
-// console.log('testing')
+
+
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+canvas.height = 1000 * devicePixelRatio;
+
 var butPushed = 0;
+var canvas = document.getElementById('firstCanvas');
+var c = canvas.getContext('2d');
+c.strokeStyle = 'rgb(200, 0,0)';
+c.lineWidth= 1;
+
+var count = 0;
+var x=-7.13
+var y=-7.11
+var z=25.41
+var b = 0;
+var g = 0;
+var r = 0;
+var dt = 0.005;
+a = Promise.resolve()
+
+
+
+
+
+
+
+//Functions
+
+
+
 const butClick = () => {
     butPushed = 1;
 }
-var canvas = document.getElementById('firstCanvas');
-canvas.width = window.innerWidth;
-// console.log('window.innerWidth: ' + window.innerWidth); //InnerWidth = 18
-canvas.height = window.innerHeight;
-canvas.height = 1000 * devicePixelRatio;
-console.log(canvas);
 
-var c = canvas.getContext('2d');
-// c.scale(0.5, 0.5);
+
+const normalizeX = (x) =>{
+    return (window.innerWidth/2 + 30*x)
+}
+const normalizeY = (y) =>{
+    return (window.innerHeight/2 - 20*(y-15))
+}
+
 c.beginPath();
 
-c.strokeStyle = 'rgb(200, 0,0)';
-c.lineWidth= 1;
+
 const command = (func, x, y, k) => {
     return new Promise((resolve) => {
         setTimeout(() => {
@@ -24,32 +54,15 @@ const command = (func, x, y, k) => {
         }, 1000);
     });
 };
-// a = command(move, 10, 100).then(() => {console.log("Done I guess!");})
-// .then(() => {command(move, 10, 300)});
 
-a = Promise.resolve()
 
-const normalizeX = (x) =>{
-    return (window.innerWidth/2 + 30*x)
-}
-const normalizeY = (y) =>{
-    return (window.innerHeight/2 + 30*(y-15))
-}
 
-// c.moveTo(0, 0);
+
 c.moveTo(window.innerWidth/2,window.innerHeight/2);
-// c.lineTo(100, 100);
 
-// c.stroke();
-// c.moveTo(100, 100);
-// c.lineTo(100, 200);
-// c.moveTo(200, 100
-// c.stroke();
-var count = 0;
+
 var prev_pos = {x:normalizeX(y), y:normalizeY(z)};
 const move = (x, y, col) => {
-    // console.log(x, y);
-    // c.beginPath();
     if(count=1){
         c.beginPath()
         c.strokeStyle = `rgb(${col.r}, 0, ${col.b})`;
@@ -60,7 +73,6 @@ const move = (x, y, col) => {
     }
     c.moveTo(prev_pos.x, prev_pos.y);
     
-    // console.log("Stroke style is: " + c.strokeStyle);
     c.lineTo(x, y);
     c.stroke();
     c.moveTo(x, y);
@@ -72,27 +84,14 @@ const move = (x, y, col) => {
 
 
 
-// // move(100,300);
 
-// c.moveTo(normalizeX(0), normalize(0));
-// move(normalizeX(100+33), normalize(0));
-// c.moveTo(normalize((33)), normalize(0));
-// move(normalize(33), normalize(100));
-var x=1.960
-var y=2.04
-var z=12.51
-var b = 0;
-var g = 0;
-var r = 0;
-c.moveTo(normalizeX(y), normalizeY(z));
-var dt = 0.0005;
-const dxdt=(x,y,z) => {return 400*(y-x)}
-const dydt=(x,y,z)=> {return -120*x-10*(x*z) +280*y}
-const dzdt=(x,y,z) => {return 10*x*y  - 30*z}
 
-//[  0.6911211,  0.5110536, -0.5110536;
-// -0.5110536,  0.8455606,  0.1544394;
-// 0.5110536,  0.1544394,  0.8455606 ]
+// c.moveTo(normalizeX(y), normalizeY(z));
+
+const dxdt=(x,y,z) => {return 10*(y-x)}
+const dydt=(x,y,z)=> {return x*(28-z)-y}
+const dzdt=(x,y,z) => {return x*y-8*z/3}
+
 const a11 = 0.6911211, a12 = 0.5110536, a13 = -0.5110536;
 const a21 = -0.5110536, a22 = 0.8455606, a23 = 0.1544394;
 const a31 = 0.5110536, a32 = 0.1544394, a33 = 0.8455606;
@@ -101,37 +100,28 @@ const rotate = (x, y, z) => {
     const newX = a11 * x + a12 * y + a13 * z;
     const newY = a21 * x + a22 * y + a23 * z;
     const newZ = a31 * x + a32 * y + a33 * z;
-    // return { x: newX, y: newY, z: newZ };
     return {x:x, y:y, z:z}
 };
 for(let i=0; i<100000; i++){
     
-    // a = a.then(()=>{command(move, (x*i), y*i*i)}).then(() => {console.log("Done I guess!")});
     a = a.then(
             () => {
                 rotatedAxes = rotate(x,y,z);
-                // c.strokeStyle = `rgb(${r}, 0, ${b})`;
-                command(move, normalizeX(rotatedAxes.y), normalizeY(rotatedAxes.z), {r:r, g:g, b:b});
-                // command(move, normalizeX(y), normalizeY(z));
-                // console.log(normalizeX(rotatedAxes.y));
+                command(move, normalizeX(rotatedAxes.x), normalizeY(rotatedAxes.z), {r:r, g:g, b:b});
             }
         )
         .then(
             () => {
-                // console.log("Done I guess!");
                 
                 return new Promise(
                     (resolve) => {
                         var dfxdt = dxdt(x, y, z);
                         var dfydt = dydt(x, y, z);
                         var dfzdt = dzdt(x, y, z);
-                        // console.log(dxdt(x,y,z));
-                        let vel = ((dfxdt**2 + dfydt**2 + dfzdt**2)**0.5)/2000;
-                        // console.log(vel);
+                        let vel = ((dfxdt**2 + dfydt**2 + dfzdt**2)**0.5)/100;
                         r = Math.round(240*vel);
                         b = Math.round(240* (1.5-vel));
                         c.strokeStyle = `rgb(${r}, 0, ${b})`;
-                        // console.log(c.strokeStyle);
                         
 
                         x = x+(dfxdt*dt);
